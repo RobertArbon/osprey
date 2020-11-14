d2a86d40430eebfcf36e85143792646643d42a69
rob arbon <robert.arbon@bristol.ac.uk>
2020-11-14 18:31:34 +0000

    typo in repo url

--- devtools/travis-ci/install_miniconda.sh
+++ devtools/travis-ci/install_miniconda.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 MINICONDA=Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_REPO=https://repo.anaconda.com/miniconda
-MINICONDA_MD5=$(curl -s $MINICONDA_REPO | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-
+MINICONDA_MD5=$(curl $MINICONDA_REPO/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget $MINICONDA_REPO/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
